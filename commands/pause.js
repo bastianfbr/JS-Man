e0ce3593bd9b76fd6873d4642e82f2a6cf3938fa
bastianfbr <badmaxyt@gmail.com>
2021-06-21 15:25:29 +0200

    Commande Pause

---
+++ commands/pause.js
@@ -0,0 +1,25 @@
+let moment = require("moment");
+const fs = require("fs");
+const database = require("../pause.json"); typeof database;
+
+module.exports = {
+    name: "pause",
+    util: "js.pause <@Membre> <nombre_de_jours>",
+    async execute(message, args) {
+        moment.locale("fr");
+        auth = message.author;
+        let member = message.mentions.members.first();
+        message.delete();
+        if (member === undefined) {
+            message.reply(`ce membre n'existe pas ou tu ne l'as pas correctement écrit \n Rappel d'utilisation de la commande : ${this.util}`);
+            return;
+        }
+        let role = message.guild.roles.cache.find(r => r.name === "Pause");
+        let date_now = moment();
+        let date_after = date_now.clone().add(parseInt(args[1]), 'day').format('L');
+        member.roles.add(role);
+        message.channel.send(`${member} est maintenant en pause jusqu'au ${date_after}`);
+        database[member.id] = date_after; 
+        fs.writeFileSync("./pause.json", JSON.stringify(database));
+    },
+};
